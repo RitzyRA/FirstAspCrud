39d91271a96b580fac0c62a397cf3d7f799e1e05
Mindaugas <darbas.mindaugas@gmail.com>
2021-10-23 16:37:09 +0300

    Adding project with unfinished bootstrap5 migration

---
+++ Controllers/SomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApp2.Controllers
+{
+    public class SomeController : Controller
+    {
+        public string MyMethod()
+        {
+            return "I pink up the phone and say yellow!";
+        }
+    }
+}
